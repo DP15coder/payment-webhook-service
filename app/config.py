6e80638ee69fde8d3bb6b3cb3c900d7c5ad82d4d
Draksha Patle <drakshapatle15@gmail.com>
2026-02-18 23:55:38 +0530

    add config changes

--- app/config.py
+++ app/config.py
@@ -1,3 +1,23 @@
+# import os
+# from functools import lru_cache
+# from pydantic import BaseModel
+
+
+# class Settings(BaseModel):
+#     app_name: str = "Transaction Webhook Service"
+#     environment: str = os.getenv("ENVIRONMENT", "local")
+#     database_url: str = os.getenv(
+#         "DATABASE_URL",
+#         "sqlite:///./transactions.db",
+#     )
+#     redis_url: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+#     api_prefix: str = "/v1"
+
+
+# @lru_cache
+# def get_settings() -> Settings:
+#     return Settings()
+
 import os
 from functools import lru_cache
 from pydantic import BaseModel
@@ -6,9 +26,9 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     app_name: str = "Transaction Webhook Service"
     environment: str = os.getenv("ENVIRONMENT", "local")
-    database_url: str = os.getenv(
-        "DATABASE_URL",
-        "sqlite:///./transactions.db",
+    database_url: str = (
+        os.getenv("DATABASE_URL", "sqlite:///./transactions.db")
+        .replace("postgres://", "postgresql://", 1)
     )
     redis_url: str = os.getenv("REDIS_URL", "redis://localhost:6379/0")
     api_prefix: str = "/v1"
@@ -17,4 +37,3 @@ class Settings(BaseModel):
 @lru_cache
 def get_settings() -> Settings:
     return Settings()
-
